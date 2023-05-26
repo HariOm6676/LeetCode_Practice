08e5f70dcf73b849aacb9fc44c25f9555067b22a
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-05-26 19:25:42 +0530

    Time: 1 ms (47.68%), Space: 43.1 MB (9.54%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -14,49 +14,22 @@
  * }
  */
 class Solution {
-    int h;
-    List<List<TreeNode>> t1=new  ArrayList<List<TreeNode>>();
-   int dfs1(TreeNode root)
+    int dfs(TreeNode root)
     {
-      if(root!=null)
-      {
-        return Math.max(dfs1(root.left),dfs1(root.right))+1;
-      }
-      return 0;
-    }
-    void dfs(TreeNode root,List<TreeNode> t)
-    {
-      if(root!=null)
-      {
-        t.add(root);
-        if(t.size()==h)
-        t1.add(new ArrayList(t));
-        dfs(root.left, new ArrayList(t));
-        dfs(root.right,new ArrayList(t));
-      }
+       if(root==null)
+       return 0;
+       return Math.max(dfs(root.left),dfs(root.right))+1;
     }
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-      List<TreeNode> t= new ArrayList<TreeNode>();
-      h=dfs1(root);
-      dfs(root,t);
-      System.out.println(t1);
-      int i,c=0;
-      for( i=0;i<t1.get(0).size();i++)
-      {
-        TreeNode k=t1.get(0).get(i);
-        for(int j=1;j<t1.size();j++)
-        {
-          if(t1.get(j).get(i)!=k)
-          {
-            c=1;
-            break;
-          }
-        }
-        if(c==1)
-        break;
-
-      }
-
-      return t1.get(0).get(i-1);  
-    }
+        if(root==null)
+        return null;
+        int left=dfs(root.left);
+        int right=dfs(root.right);
+        if(left==right)
+        return root;
+        if(left>right)
+        return lcaDeepestLeaves(root.left);
+        else
+        return lcaDeepestLeaves(root.right);
+}
 }
