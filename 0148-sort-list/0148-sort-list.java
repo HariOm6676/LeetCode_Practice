e0516784a6aaa654c783630e5d255d6ea2449ed3
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-09-28 12:17:52 +0530

    Time: 9 ms (89.33%), Space: 56 MB (45.09%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode MergeList(ListNode l1, ListNode l2)
+    {
+        if (l1==null)
+        return l2;
+        if (l2==null)
+        return l1;
+        ListNode dummy= new ListNode();
+        ListNode c1=l1;
+        ListNode c2=l2;
+        ListNode pre=dummy;
+        while( c1!= null && c2!=null)
+        {
+            if (c1.val<=c2.val)
+            {
+                pre.next=c1;
+                c1=c1.next;
+            }
+            else{
+                pre.next=c2;
+                c2=c2.next;
+            }
+            pre=pre.next;
+        }
+        if (c1==null)
+        pre.next=c2;
+        else
+        pre.next=c1;
+        return dummy.next;
+    }
+    public ListNode MergeList2(ListNode l1, ListNode l2)
+    {
+        if (l1==null)
+        return l2;
+        if (l2==null)
+        return l2;
+        ListNode c1=l1;
+        ListNode c2=l2;
+        ListNode res=new ListNode();
+        if (c1.val<=c2.val)
+        {
+            res=c1;
+            res.next=MergeList2(l1.next,l2);
+        }
+        else{
+            res=c2;
+            res.next=MergeList2(l1,l2.next);
+        }
+        return res;
+    }
+    ListNode GetMiddle(ListNode head)
+    {
+        ListNode fast=head;
+        ListNode slow=head;
+        while ( fast.next!=null && fast.next.next!=null )
+        {
+        slow=slow.next;
+        fast=fast.next.next;
+        }
+        ListNode temp=slow.next;
+        slow.next=null;
+        return temp;
+    }
+    public ListNode sortList(ListNode head) {
+        if (head==null || head.next==null)
+        return head;
+        ListNode hah=GetMiddle(head);
+        // return hah;
+        ListNode left=sortList(head);
+        ListNode right=sortList(hah);
+        return MergeList(left,right);
+
+
+    }
+}
